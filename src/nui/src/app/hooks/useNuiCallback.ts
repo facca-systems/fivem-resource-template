7e435456858feb24ce9bef86ce92481ebe291748
Rafael <43618377+HeyyCzer@users.noreply.github.com>
2025-01-22 20:20:03 -0300

    fix endless re-renders

--- src/nui/src/app/hooks/useNuiCallback.ts
+++ src/nui/src/app/hooks/useNuiCallback.ts
@@ -8,27 +8,30 @@ export function useNuiCallback<CallbackResultType>({
 	mockData,
 }: {
 	path: string;
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 	payload?: any;
 	mockData?: CallbackResultType;
 	delay?: number;
 }) {
 	const [data, setData] = useState<CallbackResultType | null>(null);
 	const [error, setError] = useState<false | Error>(false);
-	const [isLoading, setIsLoading] = useState(true);
+	const [isLoading, setIsLoading] = useState(false);
 
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
 	const fetchData = useCallback(async () => {
 		setIsLoading(true);
 
 		try {
 			const response = await fetchNui({ path, payload, mockData, delay });
-
 			setData(response);
+
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 		} catch (error: any) {
 			setError(error);
 		} finally {
 			setIsLoading(false);
 		}
-	}, [path, delay, mockData, payload]);
+	}, [path, payload, delay]);
 
 	useEffect(() => {
 		fetchData();
