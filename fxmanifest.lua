381bdfb37d4b92db9a4fa94a4eaa56902b48bd9d
Miguel Facca <79928127+snowzitcho@users.noreply.github.com>
2025-01-22 18:56:21 -0300

    refactor fxmanifest authors

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version "cerulean"
 game "gta5"
 lua54 "yes"
 
-author "HeyyCzer"
+author "HeyyCzer & Miguel Facca (HeyyShop & Facca Systems) // discord: heyyczer | faccamiguel"
 version "1.0.0"
 description "Basic boilerplate"
 
